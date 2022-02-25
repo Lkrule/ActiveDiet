e2af2010170254493d985fd822d8d07aa0385323
Maoz <maoz123@live.com>
2022-02-26 01:50:33 +0200

    test

---
+++ app/src/main/java/com/example/activediet/WelcomeFragment.kt
@@ -0,0 +1,36 @@
+package com.example.activediet
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.activediet.databinding.FragmentWelcomeBinding
+
+
+class WelcomeFragment : Fragment() {
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.welcomeNextBtn.setOnClickListener {
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToCalculatorFragment()
+            findNavController().navigate(action)
+        }
+    }
+}
