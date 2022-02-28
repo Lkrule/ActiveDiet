aa2b689a626a2f845b00891a61a65ba7b0854da0
Maoz <maoz123@live.com>
2022-02-28 23:45:43 +0200

    add drawer menu button in action bar

--- app/src/main/java/com/example/activediet/MainActivity.kt
+++ app/src/main/java/com/example/activediet/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.activediet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.activediet.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-    // maoz write that sentence
+    // drawer initialize
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+
+        drawerLayout = binding.drawerLayout
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        // return navController.navigateUp()
+        return NavigationUI.navigateUp(navController,drawerLayout)
     }
 }
