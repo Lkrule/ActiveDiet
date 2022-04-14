29c714f65df2809753770fedddace3866c2fcce2
Liam <kremeliam@gmail.com>
2022-04-14 22:47:31 +0300

    Added hyper links+fragment navigation options, changed to constraints layout.

--- app/src/main/java/com/example/activediet/fragments/BlankFragment.kt
+++ app/src/main/java/com/example/activediet/fragments/BlankFragment.kt
@@ -1,13 +1,22 @@
 package com.example.activediet.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ListView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.activediet.R
+import com.example.activediet.databinding.FragmentBlankBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,44 +29,58 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class BlankFragment : Fragment() {
-    private var Button = null
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentBlankBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        binding = FragmentBlankBinding.inflate(layoutInflater)
+
+
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+    ): View {
+        // If we want to move to other fragments from menu
+
+
+        val button3 = binding.calculateNav
+        button3.setOnClickListener{
+            val action = BlankFragmentDirections.actionBlankFragmentToCalculatorFragment()
+            findNavController().navigate(action)
+        }
+        val button4 = binding.searchNav
+        button4.setOnClickListener{
+            val sdf = SimpleDateFormat("dd/M/yyyy")
+            val currentDate = sdf.format(Date())
+            val action = BlankFragmentDirections.actionBlankFragmentToSearchFragment(currentDate)
+            findNavController().navigate(action)
+        }
+        val button5 = binding.dailyNav
+        button5.setOnClickListener{
+            val action = BlankFragmentDirections.actionBlankFragmentToDailyFragment()
+            findNavController().navigate(action)
+        }
+        // If we want to do Hyperlinks from menu
+        val workOut_link = binding.workoutId
+        workOut_link.setOnClickListener {printText()}
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BlankFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    public fun printText() {
+        startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse
+                    ("https://www.masterclass.com/articles/full-body-workout-plan#how-to-do-a-fullbody-workout")
+            )
+        )
     }
 }
+
+
