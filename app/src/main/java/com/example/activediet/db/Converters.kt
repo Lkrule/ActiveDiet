112e9ea062bcc1e7874bb5fcff54f6fe61f46262
Maoz <maoz123@live.com>
2022-04-23 18:36:51 +0300

    test

--- app/src/main/java/com/example/activediet/db/Converters.kt
+++ app/src/main/java/com/example/activediet/db/Converters.kt
@@ -1,8 +1,11 @@
 package com.example.activediet.db
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.room.TypeConverter
 import com.example.activediet.data.Nutrients
 import com.google.gson.Gson
+import java.io.ByteArrayOutputStream
 
 class Converters {
     @TypeConverter
@@ -13,5 +16,17 @@ class Converters {
     @TypeConverter
     fun nutrientsToString(nutrients: Nutrients): String {
         return Gson().toJson(nutrients)
+    }
+
+    @TypeConverter
+    fun toBitmap(bytes: ByteArray): Bitmap {
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
+
+    @TypeConverter
+    fun fromBitmap(bmp: Bitmap): ByteArray {
+        val outputStream = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        return outputStream.toByteArray()
     }
 }
