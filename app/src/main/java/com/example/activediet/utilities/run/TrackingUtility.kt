5190a160230c4ae9b64f974a2644aea4f1884ec7
Maoz <maoz123@live.com>
2022-05-27 13:43:55 +0300

    test

--- app/src/main/java/com/example/activediet/utilities/run/TrackingUtility.kt
+++ app/src/main/java/com/example/activediet/utilities/run/TrackingUtility.kt
@@ -25,25 +25,6 @@ object TrackingUtility {
             )
         }
 
-/*    fun calculatePolylineLength(polyline: Polyline): Float{
-        var distance = 0f
-        for (i in 0..polyline.size - 2){
-            val pos1 = polyline[i]
-            val pos2 = polyline[i + 1]
-
-            val result = FloatArray(1)
-            Location.distanceBetween(
-                pos1.latitude,
-                pos1.longitude,
-                pos2.latitude,
-                pos2.longitude,
-                result
-            )
-            distance += result[0]
-        }
-        return  distance
-    }*/
-
     fun getFormattedStopWatchTime(ms: Long, includeMillis: Boolean = false): String{
         var milliseconds = ms
         val hours = TimeUnit.MILLISECONDS.toHours(milliseconds)
